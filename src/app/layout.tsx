b7e640df6921e3e15a9f9a926fa8dedca4794d3b
Ahsan <ahsanaz461@gmail.com>
2024-10-21 09:47:29 +0700

    Add shadcn's UIs and GoogleAnalytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,7 @@
-import type { Metadata } from "next";
+import NextAuthProvider from "@/components/providers/NextAuthProvider";
+import { Toaster } from "@/components/ui/sonner";
+import { GoogleAnalytics } from "@next/third-parties/google";
+import type { Metadata, Viewport } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 
@@ -13,13 +16,27 @@ const geistMono = localFont({
   weight: "100 900",
 });
 
+const robots =
+  process.env.APP_ENV != "production" ? "noindex, nofollow" : "index, follow";
+
 // TOOD: Change this metadata
 export const metadata: Metadata = {
-  title: "My App",
+  title: { default: "My App", template: "%s | My App" },
   description:
     "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sint, porro.",
   authors: [{ name: "Ahsan Azizan", url: "https://ahsanzizan.xyz/" }],
+  creator: "My Team",
+  publisher: "My Publisher",
+  openGraph: {
+    images: `${process.env.URL}/logo-horizontal.png`,
+  },
   keywords: ["my", "app"],
+  robots,
+};
+
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
 };
 
 export default function RootLayout({
@@ -28,12 +45,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
+    <NextAuthProvider>
+      <html lang="en">
+        {process.env.APP_ENV === "production" && process.env.GA_ID && (
+          <GoogleAnalytics gaId={process.env.GA_ID} />
+        )}
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased overflow-x-hidden`}
+        >
+          <main>{children}</main>
+          <Toaster />
+        </body>
+      </html>
+    </NextAuthProvider>
   );
 }
