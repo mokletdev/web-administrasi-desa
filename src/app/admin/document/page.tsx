677fa74ee684487cc66c8bca66a777f931903529
Ahsan <ahsanaz461@gmail.com>
2024-11-10 09:34:32 +0700

    Follow up

--- src/app/admin/document/page.tsx
+++ src/app/admin/document/page.tsx
@@ -1,3 +1,27 @@
+import prisma from "@/lib/prisma";
+import { DocumentTable } from "./components/document-table";
+
 export default async function Document() {
-  return <></>;
+  const [documents, fieldTypes] = await prisma.$transaction([
+    prisma.document.findMany({
+      include: { user: { select: { name: true } } },
+    }),
+    prisma.fieldType.findMany({
+      select: { id: true, name: true, baseType: true },
+    }),
+  ]);
+
+  return (
+    <>
+      <div className="mb-8 flex flex-col gap-2">
+        <h1>Manajemen Template Surat</h1>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing el it. Illum
+          necessitatibus corporis debitis praesentium unde assumenda itaque ipsa
+          earum ipsum tempore!
+        </p>
+      </div>
+      <DocumentTable documents={documents} fieldTypes={fieldTypes} />
+    </>
+  );
 }
